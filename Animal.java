20674750a9354bb370c66eaadf2cbea682eec61f
Diego Uslar Castillo <diegouslarcastillo21@gmail.com>
2025-09-10 18:12:29 -0400

    Delete Animal.java

--- Animal.java
+++
@@ -1,9 +0,0 @@
-public class Animal {
-    protected  String nombre;
-    public Animal( String nombre){
-        this.nombre= nombre;
-    }
-    void hacerSonido(){
-        System.out.println("El animal hace un sonido");
-    }
-}
