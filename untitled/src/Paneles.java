c2ed8f969416e952d495912ff01ceb47ab8b6260
Diego Uslar Castillo <diegouslarcastillo21@gmail.com>
2025-09-30 09:07:40 -0400

    Add files via upload

---
+++ untitled/src/Paneles.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Paneles extends JFrame {
+    public  Paneles(){
+        setTitle("Teclado");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+        JPanel teclado= new Teclado();
+        JPanel funciones= new Funciones();
+        JLabel titulo= new JLabel("Teclado");
+        add(teclado, BorderLayout.CENTER);
+        add(titulo, BorderLayout.NORTH);
+        add(funciones, BorderLayout.EAST);
+        add(new JButton("Aceptar"), BorderLayout.SOUTH);
+        setVisible(true);
+    }
+}
